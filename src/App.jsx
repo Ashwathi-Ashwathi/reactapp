71f1efbc1d7a630cf43d64474a7260eb2d674ac0
Ashwathi-Ashwathi <ashwathinambiar4321@gmail.com>
2023-07-05 19:30:05 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,58 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import im from './assets/mri.png'
-
+import Home from './pages/Home'
+import Login from './pages/Login'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div class="container">
-
-        <nav>
-          <img src={im} class="logo"></img>
-
-          <ul>
-
-            <li> <a href="#">Premium</a></li>
-            <li> <a href="#">Help</a></li>
-            <li> <a href="#">Download</a></li>
-            <li> <a href="#">|</a></li>
-            <li> <a href="./login.html">Signup</a></li>
-            <li> <a href="login.html">LogIn</a></li>
-          </ul>
-        </nav>
-        <div class="container2">
-          <div class="music">
-            <h1>
-              <bold>
-
-                Music for everyone.<br></br>
-              </bold>
-            </h1>
-
-            <div class="millions">
-
-              <bold>
-                <h4>Millions of songs.No credit card needed.</h4>
-              </bold>
-            </div>
-
-            <button class="butt">GET SPOTIFY FREE</button>
-
-
-
-          </div>
-        </div>
-
-
-
-
-
-      </div >
-
-
+      <Routes>
+        <Route path='/' exact element={<Home />}></Route>
+        <Route path='/Login' exact element={<Login />}></Route>
+      </Routes>
     </>
   )
 }
